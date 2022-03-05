26a03baf962dd8a69bd0a6be68163b0ab00dfe9a
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-03-04 18:13:44 -0600

    created function to generate number based on prompt response

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -16,7 +16,6 @@ var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-",
 
 
 // Provided by instructor
-
 // ask about Uppercase
 function askAboutUppercase(){
   allowUppercase = confirm("Are upper case characters allowed?");
@@ -41,8 +40,37 @@ function askAboutSpecials(){
   console.log(allowSpecials);
 }
 // Repeat for other criteria; for the number of characters, use a prompt statement. Google it. NOTE: prompt statements think any value you provide is a string. You will need to convert it to a number.
+
+//this will make numberGiven whatever number is entered into the prompt.
 function askNumberOfCharacters(){
-  window.prompt("How many characters are allowed?");
+  var numberGiven = prompt("How many characters are allowed?"); //allows user to input number of characters
+  if (numberGiven === null){ 
+    return; //this will break the loop if cancel button is pressed.
+  }else if ( numberGiven > 0 && numberGiven < 26) { //the number needs to be above 0
+    console.log(numberGiven); //just here to verify number
+    console.log(typeof numberGiven); //just here to verify data type
+    var numberGivenInt = parseInt(numberGiven); //this will pass the numberGiven from a string to a number.
+    console.log(typeof numberGivenInt); //just here to verify data type
+  }else {
+    askNumberofCharacters0to25()
+  }
+  
+}
+
+//this function will create a loop requiring the user to input a number above 0.
+function askNumberofCharacters0to25(){
+  var numberGiven = prompt("Please enter a number between 0 and 25 for the allowed charaters. Thank You!"); 
+  //allows user to input a different number of characters allowed above 0.
+  if (numberGiven === null){
+    return;
+  }else if ( numberGiven > 0 && numberGiven < 26) { //the number needs to be above 0
+    console.log(numberGiven); //just here to verify the number
+    var numberGivenInt = parseInt(numberGiven); //this will pass the numberGiven from a string to a number.
+    console.log(typeof numberGivenInt); //just here to verify data type
+  } else {
+    askNumberOfCharacters ();
+  }
+
 }
 
 // Once all the criteria are determined, this function will generate the password. You can create other functions also if you need them.
@@ -50,6 +78,11 @@ function generatePassword(){
   var finalResult = ""
 
   // HINT: Remember that a for-loop can iterate from a starting number to a ending number, such as the number of characters in a password.
+
+  //****************************************************figure out this for loop
+  // for (let i=0; len = uppercaseChars; i++){
+  //   var uppercaseRandom = i + "";   
+  // }
 
   // HINT: You may want to look into merging arrays together
 
@@ -78,4 +111,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-drawbox(red);
